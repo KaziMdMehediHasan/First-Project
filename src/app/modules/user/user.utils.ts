c6d5d7a1fa137be67cd6f27b28f0e646f4b180ed
Kazi Mehedi Hasan <hassan.mehedee92@gmail.com>
2024-05-30 20:12:34 +0600

    id bug fixed with the help of 13-1

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -11,13 +11,26 @@ const findLastStudentId = async () => {
         .lean();
 
     //203001  0001
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    // return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent.id : undefined;
 }
 // year, semesterCode, 4 digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
-    console.log(await findLastStudentId());
+    // console.log(await findLastStudentId());
     // first time id 0000
-    const currentId = await findLastStudentId() || (0).toString();
+    // const currentId = await findLastStudentId() || (0).toString();
+    let currentId = (0).toString(); //0000 by default
+    const lastStudentId = await findLastStudentId();
+    const lastStudentYear = lastStudentId?.substring(0, 4);
+    const lastStudentIdSemesterCode = lastStudentId?.substring(4, 6);
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    // checks if the semester and year are same as the previous student then takes that id as a reference to assign a new id for the new student in the same session
+    if (lastStudentId && lastStudentIdSemesterCode === currentSemesterCode && lastStudentYear === currentYear) {
+        currentId = lastStudentId?.substring(6);
+    }
+
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
     incrementId = `${payload.year}${payload.code}${incrementId}`;
